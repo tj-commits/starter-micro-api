a3dace6caf7ef8ae9d2a1facc2ebdd468d871368
tj-commits <158522817+tj-commits@users.noreply.github.com>
2024-02-01 16:01:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const io = require('socket.io')(3000, {
+  cors: {
+    origin: true
+  }
+})
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
